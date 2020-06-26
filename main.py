db7d7e989fec6a73871412365302caa4b182e6ec
Lenivius <241978@student.pwr.edu.pl>
2020-06-26 22:14:02 +0200

    final StreamGenerator values, added 'charts' folder

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import strlearn as sl
 import numpy as np
 from ourAWE import OurAWE
@@ -26,61 +27,67 @@ clfs = [
     sl.ensembles.SEA(GaussianNB()),
     sl.ensembles.AUE(GaussianNB()),
     sl.ensembles.WAE(GaussianNB()),
-    sl.ensembles.AWE(GaussianNB())
-    #OurAWE(GaussianNB())
+    sl.ensembles.AWE(GaussianNB()),
+    OurAWE(GaussianNB())
 ]
 clf_names = [
     'SEA',
     'AUE',
     'WAE',
-    'AWE'
-    #'OurAWE'
+    'AWE',
+    'OurAWE'
 ]
+
+""" KONTROLA BUDOWY STRUMIENIA DANYCH """
+n_chunks = 200              #### LICZBA BLOKÓW DANYCH
+chunk_size = 250            #### ROZMIAR BLOKU DANYCH
+n_drifts = 3                #### LICZBA WYSTĄPIENIA DRYFU KONCEPCJI W STRUMIENIU
+
 
 alfa = .05                  #### PRÓG NIEPEWNOŚCI
 scores = []                 #### TABLICA PRZECHOWUJĄCA WYNIKI POSZCZEGÓLNYCH EWALUACJI
 streams_names = dict([      #### SŁOWNIK WYKORZYSTYWANY DO ZACHOWANIA CZYTELNOŚCI PREZENTACJI DANYCH
     (0, "STRUMIENI Z DRYFEM NAGŁYM"), 
-    (1, "STRUMIENI Z DRYFEM DUALNY"), 
-    (2, "STRUMIENI Z DRYFEM INKREMENTALNY")
+    (1, "STRUMIENI Z DRYFEM DUALNYM"), 
+    (2, "STRUMIENI Z DRYFEM INKREMENTALNYM")
 ])
-streams_list = [[] for i in range (3)]  #### LISTA TRZECH LIST ZAWIERAJĄCYCH STRUMIENIE DANYCH
+streams_list = [[] for i in range (3)]  #### LISTA TRZECH LIST DO PRZECHOWYWANIA STRUMIENI DANYCH
 
 
 """ GENEROWANIE STRUMIENI DANYCH """
 for rstate in rand_states:
     stream = sl.streams.StreamGenerator(
-        n_chunks = 200,
-        chunk_size = 250,
+        n_chunks = n_chunks,
+        chunk_size = chunk_size,
         random_state = rstate,
         n_features = 10,
         n_classes = 2,
-        n_drifts = 1
+        n_drifts = n_drifts
     )
     streams_list[0].append(stream)      #### LISTA STRUMIENI Z DRYFEM NAGŁYM
 
     stream = sl.streams.StreamGenerator(
-        n_chunks = 200,
-        chunk_size = 250,
+        n_chunks = n_chunks,
+        chunk_size = chunk_size,
         random_state = rstate,
         n_features = 10,
         n_classes = 2,
-        n_drifts = 1,
+        n_drifts = n_drifts,
         concept_sigmoid_spacing = 5
     )
-    streams_list[1].append(stream)      #### LISTA STRUMIENI Z DRYFEM DUALNY
+    streams_list[1].append(stream)      #### LISTA STRUMIENI Z DRYFEM DUALNYM
 
     stream = sl.streams.StreamGenerator(
-        n_chunks = 200,
-        chunk_size = 250,
+        n_chunks = n_chunks,
+        chunk_size = chunk_size,
         random_state = rstate,
         n_features = 10,
         n_classes = 2,
-        n_drifts = 1,
+        n_drifts = n_drifts,
         concept_sigmoid_spacing = 5,
         incremental = True
     )
-    streams_list[2].append(stream)      #### LISTA STRUMIENI Z DRYFEM INKREMENTALNY
+    streams_list[2].append(stream)      #### LISTA STRUMIENI Z DRYFEM INKREMENTALNYM
 
 
 """ WYKORZYSTYWANE METRYKI """
@@ -112,7 +119,6 @@ for st_id, stream_ in enumerate(streams_list):
         avg_mean_scores.append(np.mean(ms, axis = 0))
 
     """ PAROWE TESTY STATYSTYCZNE """
-
     print("\n -----||| ANALIZA STATYSTYCZNA DLA {} |||-----\n".format(streams_names[st_id]))
     t_statistic = np.zeros((len(clfs), len(clfs)))
     p_value = np.zeros((len(clfs), len(clfs)))
@@ -122,7 +128,7 @@ for st_id, stream_ in enumerate(streams_list):
             t_statistic[i, j], p_value[i, j] = ttest_ind(avg_mean_scores[i], avg_mean_scores[j])
 
     """ TWORZENIE TABELI FORMATUJĄCEJ WYNIKI """
-    names_column = np.array([["SEA"], ["AUE"], ["WAE"], ["AWE"]])
+    names_column = np.array([["SEA"], ["AUE"], ["WAE"], ["AWE"], ["OurAWE"]])
     t_statistic_table = np.concatenate((names_column, t_statistic), axis=1)
     t_statistic_table = tabulate(t_statistic_table, clf_names, floatfmt=".2f")
     p_value_table = np.concatenate((names_column, p_value), axis=1)
@@ -147,7 +153,7 @@ for st_id, stream_ in enumerate(streams_list):
     print("Lepszy znacząco statystycznie:\n", stat_better_table, "\n")
 
     """ RYSOWANIE WYKRESU WYNIKÓW EWALUACJI """
-    fig, ax = plt.subplots(1, len(metrics), figsize=(24, 8))
+    fig, ax = plt.subplots(1, len(metrics), figsize=(24, 8), num = "Wykresy dla {}".format(streams_names[st_id].lower()))
     for m, metric in enumerate(metrics):
         ax[m].set_title(metrics_names[m])
         ax[m].set_ylim(0, 1)
@@ -156,4 +162,8 @@ for st_id, stream_ in enumerate(streams_list):
         plt.ylabel("Mean scores of metric")
         plt.xlabel("Chunk")
         ax[m].legend()
+    if not os.path.isdir("charts"):
+        os.mkdir("charts")
+    plt.savefig("charts/fig_{}".format(st_id))
 plt.show()
+print("Aby zakończyć proces zamknij wszystkie okna z wykresami")
